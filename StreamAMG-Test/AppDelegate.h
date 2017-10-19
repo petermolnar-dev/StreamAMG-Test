a4673dd78216df150c78ed6ee421891e01b62cb1
Peter Molnar <peter@petermolnar.hu>
2017-10-19 14:13:06 +0200

    Initial Commit

---
+++ StreamAMG-Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StreamAMG-Test
+//
+//  Created by Peter Molnar on 19/10/2017.
+//  Copyright © 2017 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StreamAMG-Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StreamAMG-Test
+//
+//  Created by Peter Molnar on 19/10/2017.
+//  Copyright © 2017 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
